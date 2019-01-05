71cd4955ec2e02ef0bf1b9bd29727ab2e64e10ca
Unknown <g3agle@gmail.com>
2019-01-05 21:46:42 +0300

    Full commit of version 1.2

---
+++ app/src/main/java/com/goldeneagle/hamadcartoon/AboutActivity.java
@@ -0,0 +1,55 @@
+package com.goldeneagle.hamadcartoon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class AboutActivity extends Activity {
+
+    TextView aboutnametxt, aboutlisttxt, workslisttxt, achievementlisttxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        aboutnametxt = findViewById(R.id.About_Name);
+        aboutlisttxt = findViewById(R.id.About_List);
+        workslisttxt = findViewById(R.id.Works_List);
+        achievementlisttxt = findViewById(R.id.Achievement_List);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/gess.otf");
+        aboutlisttxt.setTypeface(typeface);
+        aboutnametxt.setTypeface(typeface);
+        workslisttxt.setTypeface(typeface);
+        achievementlisttxt.setTypeface(typeface);
+
+    }
+
+    public void AccessFacebook(View view){
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/hamad.algayeb")));
+
+    }
+
+    public void AccessInstagram(View view){
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/hamad.algayeb")));
+
+    }
+
+
+
+    public void AccessTwitter(View view){
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/hamadalgayeb")));
+
+    }
+
+}
+
+
---
+++ app/src/main/java/com/goldeneagle/hamadcartoon/MainActivity.java
@@ -0,0 +1,81 @@
+package com.goldeneagle.hamadcartoon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button browseBookbtn, aboutArtistbtn, rateAppbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        browseBookbtn = findViewById(R.id.Browse_Book);
+        aboutArtistbtn = findViewById(R.id.About_Artist);
+        rateAppbtn = findViewById(R.id.Rate_App);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/gess.otf");
+        browseBookbtn.setTypeface(typeface);
+        aboutArtistbtn.setTypeface(typeface);
+        rateAppbtn.setTypeface(typeface);
+
+
+    }
+
+    public void BrowseBook(View view) {
+        browseBookbtn.setBackgroundColor(getResources().getColor(R.color.colorButtonPressed));
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                browseBookbtn.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        }, 100);
+
+        Intent viewPDF = new Intent(Intent.ACTION_VIEW);
+        viewPDF.setDataAndType(Uri.parse("https://drive.google.com/open?id=0Byio6GQV9piwTlRkNDctS19Dd0h6QzMtaVQtS0hrRzU2Sktn"), "text/html");
+        startActivity(viewPDF);
+
+
+    }
+
+
+    public void AboutArtist(View view) {
+        aboutArtistbtn.setBackgroundColor(getResources().getColor(R.color.colorButtonPressed));
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                aboutArtistbtn.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        }, 100);
+
+    }
+
+    public void RateApp(View view) {
+        rateAppbtn.setBackgroundColor(getResources().getColor(R.color.colorButtonPressed));
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                rateAppbtn.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+        }, 100);
+
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.goldeneagle.hamadcartoon")));
+    }
+
+}
